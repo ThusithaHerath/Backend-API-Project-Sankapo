bf0af2e83387bacebd51a077fc307d447a1cf762
Thusitha Herath <83418246+ThusithaHerath@users.noreply.github.com>
2022-12-13 20:38:42 +0530

    Add products

---
+++ database/migrations/2022_12_04_152828_create_add_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('add_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('category')->index()->unsigned();
+            $table->string('description');
+            $table->string('images');
+            $table->string('condition');
+            $table->string('buy');
+            $table->boolean('isApprove')->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('add_products');
+    }
+}
